e1b37e15f2a621f379e6a5d0f633333ccaa518d1
JuanBritog <83216119+JuanBritog@users.noreply.github.com>
2022-06-17 10:28:17 +0200

    Initial commit

---
+++ src/BusinessDelegatePattern/JMSService.java
@@ -0,0 +1,8 @@
+package BusinessDelegatePattern;
+public class JMSService implements BusinessService {
+
+	   @Override
+	   public void doProcessing() {
+	      System.out.println("Processing task by invoking JMS Service");
+	   }
+	}
